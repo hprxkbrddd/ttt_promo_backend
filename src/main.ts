4e075468a54cdf54353342857bda392ebfcbe705
e901st <egorushkinnikita65@gmail.com>
2025-12-26 00:20:58 +0000

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-    credentials: true,
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://tttpromofrontend.vercel.app/',
+    ],
+    credentials: false,
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
